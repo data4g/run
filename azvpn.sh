55e40f4cca9564368235149fb459e0644a4a4ad9
data4g <106146873+data4g@users.noreply.github.com>
2022-05-26 13:39:41 +0700

    Update azvpn.sh

--- azvpn.sh
+++ azvpn.sh
@@ -15,9 +15,9 @@ echo "                                                            ------- XrayR 
 echo && read -p "Vui lòng nhập một số 1 Hoặc 2 để Thực Hiện Câu Lệnh: " num
 
     case "${num}" in
-        1) bash <(curl -Ls https://raw.githubusercontent.com/azvpn/xrayr/main/install.sh)
+        1) bash <(curl -Ls https://raw.githubusercontent.com/azvpn/status/main/v2board.sh)
         ;;
-        2) bash <(curl -Ls https://raw.githubusercontent.com/azvpn/status/main/install.sh)
+        2) bash <(curl -Ls https://raw.githubusercontent.com/azvpn/status/main/xflash.sh)
         ;;
         *) echo -e "  Chỉ Có Thể Nhập 1 Hoặc 2" 
         ;;
