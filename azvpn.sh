380b37198d383ff07bbbf73bab86cc922c2e7650
data4g <106146873+data4g@users.noreply.github.com>
2022-05-26 12:43:47 +0700

    Update azvpn.sh

--- azvpn.sh
+++ azvpn.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
+echo -e "" 
+echo "         --------------------------------------------------------------------------------------------------------------------------------------------------"
+echo -e "" 
+                                                                Vui Lòng Lựa Chọn Source !
 
-bash <(curl -Ls https://raw.githubusercontent.com/azvpn/status/main/key.sh)
+echo "         ██████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████"
+echo "         ██                                                                  ██                                                                          ██"
+echo "         ██                          1. V2BOARD                              ██                                2. XFLASH                                 ██"
+echo "         ██                                                                  ██                                                                          ██"
+echo "         ██████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████"
+
+echo -e "" 
+echo "                                                            ------- XrayR 1 Stack của long -------"
+echo && read -p "                                        Vui lòng nhập một số 1 Hoặc 2 để Thực Hiện Câu Lệnh: " num
+
+    case "${num}" in
+        1) bash <(curl -Ls https://raw.githubusercontent.com/azvpn/xrayr/main/install.sh)
+        ;;
+        2) bash <(curl -Ls https://raw.githubusercontent.com/azvpn/status/main/key.sh)
+        ;;
+        *) echo -e "  Chỉ Có Thể Nhập 1 Hoặc 2" 
+        ;;
+    esac
